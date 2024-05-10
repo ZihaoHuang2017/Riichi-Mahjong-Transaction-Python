23e310c34ac6707e3fc3ef6dd78cb0ec51085176
Zihao Huang <zihaohuang2017@gmail.com>
2024-05-09 23:58:20 -0700

    support underscore global vars, extract some methods

--- transformer.py
+++ transformer.py
@@ -11,7 +11,24 @@ from IPython.core.magic import register_line_magic
 from IPython.core.magic_arguments import magic_arguments, argument, parse_argstring
 from IPython.utils import io
 
-SET_UP_KEYWORDS = ["from", "import", "%"]
+INDENT_SIZE = 4
+
+
+class RewriteUnderscores(ast.NodeTransformer):
+    def __init__(self, one_underscore, two_underscores, three_underscores):
+        self.one_underscore = one_underscore
+        self.two_underscores = two_underscores
+        self.three_underscores = three_underscores
+
+    def visit_Name(self, node):
+        if node.id == "_":
+            return ast.Name(id=f"_{self.one_underscore}", ctx=ast.Load())
+        elif node.id == "__":
+            return ast.Name(id=f"_{self.two_underscores}", ctx=ast.Load())
+        elif node.id == "___":
+            return ast.Name(id=f"_{self.three_underscores}", ctx=ast.Load())
+        else:
+            return node
 
 
 def load_ipython_extension(ipython: IPython.InteractiveShell):
@@ -50,55 +67,83 @@ def load_ipython_extension(ipython: IPython.InteractiveShell):
             outfile = open(outfname, "w", encoding="utf-8")
             close_at_end = True
 
-        import_statements = ["import jsons"]
+        import_statements = set()
         normal_statements = []
+        output_lines = [0, 0, 0]
         histories = ipython.history_manager.get_range(output=True)
         for session, line, (lin, lout) in histories:
             try:
-                if lin.startswith("%"):  # magic methods
-                    continue
-                if lin.startswith("from ") or lin.startswith("import "):
-                    import_statements.append(lin)
-                    continue
-                if not lout:
-                    ipython.ex(lin)
-                    # not the most ideal way if we have some weird crap going on (remote apis???)
-                    normal_statements.append(lin)
-                else:
-                    obj_result = ipython.ev(lin)
-                    normal_statements.append(f"_{line} = {lin}")
-                    if obj_result is True:
-                        normal_statements.append(f"assert _{line}")
-                    elif obj_result is False:
-                        normal_statements.append(f"assert not _{line}")
-                    elif type(type(obj_result)) is enum.EnumMeta:
-                        normal_statements.append(
-                            f"assert _{line} == {str(obj_result)}"
-                        )
-                    elif type(obj_result) is type:
-                        normal_statements.append(
-                            f"assert _{line} is {obj_result.__name__}"
-                        )
-                    else:
-                        try:
-                            ipython.ev(repr(obj_result))
-                            normal_statements.append(
-                                f"assert _{line} == {repr(obj_result)}"
-                            )
-                        except SyntaxError:
-                            normal_statements.append(
-                                f"assert type(_{line}) is {obj_result.__class__.__name__}"
-                            )
-                            normal_statements.append(
-                                f"assert jsons.dump(_{line}) == {jsons.dump(obj_result)}"
-                            )
+                parse_statement(
+                    import_statements, normal_statements, output_lines, line, lin, lout
+                )
             except (SyntaxError, NameError):
+                continue
+            except Exception as e:
+                import_statements.add("import pytest")
+                normal_statements.append(f"with pytest.raises({e.__class__.__name__}):")
+                normal_statements.append(" " * INDENT_SIZE + lin)
                 continue
 
         print(*import_statements, sep="\n", file=outfile)
         print("\n", file=outfile)
         print("def test_func():", file=outfile)
         for statement in normal_statements:
-            print(" " * 4 + statement, file=outfile)
+            print(" " * INDENT_SIZE + statement, file=outfile)
         if close_at_end:
             outfile.close()
+
+    def parse_statement(
+        import_statements, normal_statements, output_lines, line, lin, lout
+    ):
+        if lin.startswith("%"):  # magic methods
+            return
+        if lin.startswith("from ") or lin.startswith("import "):
+            import_statements.add(lin)
+            return
+        revised_statement = revise_line_input(lin, output_lines)
+        if not lout:
+            ipython.ex(revised_statement)
+            normal_statements.append(revised_statement)
+            # not the most ideal way if we have some weird crap going on (remote apis???)
+            return
+        obj_result = ipython.ev(revised_statement)
+        output_lines.append(line)
+        normal_statements.append(f"_{line} = {revised_statement}")
+        if obj_result is True:
+            normal_statements.append(f"assert _{line}")
+        elif obj_result is False:
+            normal_statements.append(f"assert not _{line}")
+        elif type(type(obj_result)) is enum.EnumMeta:
+            normal_statements.append(f"assert _{line} == {str(obj_result)}")
+        elif type(obj_result) is type:
+            normal_statements.append(f"assert _{line} is {obj_result.__name__}")
+        else:
+            try:
+                ipython.ev(repr(obj_result))
+                normal_statements.append(f"assert _{line} == {repr(obj_result)}")
+            except SyntaxError:
+                normal_statements.append(
+                    f"assert type(_{line}) is {obj_result.__class__.__name__}"
+                )
+                try:
+                    serialised_obj = jsons.dump(obj_result)
+                    import_statements.add("import jsons")
+                    normal_statements.append(
+                        f"assert jsons.dump(_{line}) == {serialised_obj}"
+                    )
+                except Exception:  # 万策尽
+                    pass
+
+    def revise_line_input(lin, output_lines):
+        # Undefined Behaviour if the user tries to invoke _ with len<3. Why would you want to do that?
+        one_underscore, two_underscores, three_underscores = (
+            output_lines[-1],
+            output_lines[-2],
+            output_lines[-3],
+        )
+        node = ast.parse(lin)
+        revised_node = RewriteUnderscores(
+            one_underscore, two_underscores, three_underscores
+        ).visit(node)
+        revised_statement = ast.unparse(revised_node)
+        return revised_statement
